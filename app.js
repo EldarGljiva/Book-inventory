759a99bd301183ec10596675859c412196a5c3e3
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2023-12-08 16:52:19 +0100

    backend implemented

--- app.js
+++ app.js
@@ -1,17 +1,77 @@
 import express from "express";
 import pg from "pg";
+import axios from "axios";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-/*
-const db = new pg.Client({});
-db.connect();
-*/
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+db.connect();
+
+let books = [
+  {
+    id: 1,
+    isbn: "0385472579",
+    title: "nesta",
+    description: "nesta",
+    rating: 3,
+  },
+];
+
+app.get("/", async (req, res) => {
+  try {
+    const books = await db.query("SELECT * FROM books order by id");
+    const result = books.rows;
+    res.render("index.ejs", { books: result });
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/add", async (req, res) => {
+  try {
+    await db.query(
+      "INSERT INTO books(isbn,title, description,rating) VALUES($1,$2,$3,$4)",
+      [req.body.isbn, req.body.title, req.body.description, req.body.rating]
+    );
+    res.redirect("/");
+  } catch (err) {
+    res.status(400).send("You Entered wrong ISBN");
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const id = req.body.book_id;
+  console.log(id);
+  db.query("DELETE FROM books WHERE id = $1", [id]);
+  res.redirect("/");
+});
+
+app.post("/edit", async (req, res) => {
+  const description = req.body.updated_item_description;
+  const id = req.body.updated_book_id;
+  try {
+    await db.query("UPDATE books SET description = $1 WHERE id = $2", [
+      description,
+      id,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
